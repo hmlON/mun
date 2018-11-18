56eb0e34212e673e20fc6c2144e2827fb210e7cd
Nikita Kholin <nik.kholin@gmail.com>
2018-11-18 17:21:10 +0200

    Save and show releases from artists

--- integrations/models.py
+++ integrations/models.py
@@ -11,3 +11,11 @@ class Artist(models.Model):
     integration = models.ForeignKey(Integration, on_delete=models.CASCADE)
     integration_artist_id = models.CharField(max_length=256, null=True)
     name = models.CharField(max_length=256)
+
+class Release(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    integration_release_id = models.CharField(max_length=256, null=True)
+    title = models.CharField(max_length=256)
+    cover_url = models.CharField(max_length=256)
+    date = models.DateField()
+    release_type = models.CharField(max_length=256)
--- integrations/views.py
+++ integrations/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from integrations.models import Artist
+from integrations.models import Artist, Release
 import requests
+import logging
+logger = logging.getLogger(__name__)
 
 def index(request):
     # get integration
@@ -8,26 +10,62 @@ def index(request):
     user_id = integration.integration_user_id
 
     # load all artists
+    # TODO: use next param instead of loop
     artists = []
     all_artists_loaded = False
     index = 0
     limit = 50
 
     while not all_artists_loaded:
-      url = f"https://api.deezer.com/user/{user_id}/artists?index={index}&limit={limit}"
-      current_request_artists = requests.get(url).json()['data']
-      artists += current_request_artists
-      index += limit
-      all_artists_loaded = len(current_request_artists) < limit
+        url = f"https://api.deezer.com/user/{user_id}/artists?index={index}&limit={limit}"
+        current_request_artists = requests.get(url).json()['data']
+        logger.info(f"Getting data from {url}")
+        artists += current_request_artists
+        index += limit
+        all_artists_loaded = len(current_request_artists) < limit
 
     # save or update loaded artists
     for artist in artists:
         find_by = {"integration": integration, "integration_artist_id": artist["id"]}
+        update = {"name": artist["name"]}
         if Artist.objects.filter(**find_by).exists():
-            Artist.objects.filter(**find_by).update(name=artist["name"])
+            Artist.objects.filter(**find_by).update(**update)
         else:
-            Artist.objects.create(name=artist["name"], **find_by)
+            Artist.objects.create(**update, **find_by)
+
+    artists = integration.artist_set.all()
+
+    for artist in artists:
+        # load releases
+        releases = []
+        all_releases_loaded = False
+        index = 0
+        limit = 50
+        artist_id = artist.integration_artist_id
+
+        while not all_releases_loaded:
+              url = f"https://api.deezer.com/artist/{artist_id}/albums?index={index}&limit={limit}"
+              current_request_releases = requests.get(url).json()['data']
+              logger.info(f"Getting data from {url}")
+              releases += current_request_releases
+              index += limit
+              all_releases_loaded = len(current_request_artists) < limit
+
+        # save or update releases
+        for release in releases:
+            find_by = {"artist": artist, "integration_release_id": release["id"]}
+            update = {
+                "title": release["title"],
+                "cover_url": release["cover"],
+                "date": release["release_date"],
+                "release_type": release["type"],
+            }
+            if Release.objects.filter(**find_by).exists():
+                Release.objects.filter(**find_by).update(**update)
+            else:
+                Release.objects.create(**update, **find_by)
 
     # render
-    context = {'user': request.user, 'artists': integration.artist_set.all()}
+    releases = Release.objects.all().order_by('-date')
+    context = {'user': request.user, 'releases': releases}
     return render(request, 'artists/index.html', context)
