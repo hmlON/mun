a25b78dee5e5dac87a5ac62ebe53f2d6f34347c8
Nikita Kholin <nik.kholin@gmail.com>
2018-11-29 21:47:27 +0200

    Save Spotify client ID & access token

--- integrations/models.py
+++ integrations/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Integration(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     identifier = models.CharField(max_length=50)
+    access_token = models.CharField(max_length=256, null=True)
     refresh_token = models.CharField(max_length=256, null=True)
     integration_user_id = models.CharField(max_length=256, null=True)
 
--- integrations/pipeline.py
+++ integrations/pipeline.py
@@ -2,9 +2,11 @@ from integrations.models import Integration
 
 def save_integration(backend, user, response, *args, **kwargs):
     if backend.name == 'spotify':
+        access_token        = response['access_token']
         refresh_token       = response['refresh_token']
-        integration_user_id = None
+        integration_user_id = response['id']
     elif backend.name == 'deezer':
+        access_token        = None
         refresh_token       = None
         integration_user_id = response['id']
 
