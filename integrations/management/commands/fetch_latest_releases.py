1bb4d6b274366d8224691497a0c803f7af62705e
Nikita Kholin <nik.kholin@gmail.com>
2018-12-06 13:42:15 +0200

    Use integrations and notifications that are present

--- integrations/management/commands/fetch_latest_releases.py
+++ integrations/management/commands/fetch_latest_releases.py
@@ -16,60 +16,77 @@ class Command(BaseCommand):
 
         for user in users:
             self.stdout.write(self.style.SUCCESS(f'starting fetching for {user.email}'))
-            # DeezerFetcher.fetch(user.id)
-            SpotifyFetcher.fetch(user.id)
+
+            integration = None
+            if user.integration_set.filter(identifier='spotify').exists():
+                integration = user.integration_set.get(identifier='spotify')
+                SpotifyFetcher.fetch(user.id)
+            elif user.integration_set.filter(identifier='deezer').exists():
+                integration = user.integration_set.get(identifier='deezer')
+                DeezerFetcher.fetch(user.id)
+
+            if not integration:
+              continue
+
             self.stdout.write(self.style.SUCCESS(f'finished fetching for {user.email}'))
 
-            integration = user.integration_set.get(identifier='spotify')
-            notification = user.notification_set.get(channel='email')
 
-            new_since = notification.last_sent_at
-            # new_since = datetime.date.today() - datetime.timedelta(days=30)
-            new_releases = Release.objects.filter(
-              artist__integration_id=integration.id,
-              date__gte=new_since
-            ).order_by('-date')
+            if user.notification_set.filter(channel='email').exists():
+                notification = user.notification_set.get(channel='email')
+                new_since = notification.last_sent_at
+                # new_since = datetime.date.today() - datetime.timedelta(days=30)
+                new_releases = Release.objects.filter(
+                    artist__integration_id=integration.id,
+                    date__gte=new_since
+                ).order_by('-date')
 
 
-            # send an email
-            if new_releases.exists():
-              self.stdout.write(self.style.SUCCESS(f'sending an email for {user.email} '))
+                # send an email
+                if new_releases.exists():
+                    self.stdout.write(self.style.SUCCESS(f'sending an email for {user.email} '))
 
-              releases_text = [f'{release.date}: {release.artist.name} - {release.title}' for release in new_releases]
-              intro_text = 'Here are latest music releases that you have not seen before:'
-              email_text = '\n'.join([intro_text] + releases_text)
+                    releases_text = [f'{release.date}: {release.artist.name} - {release.title}' for release in new_releases]
+                    intro_text = 'Here are latest music releases that you have not seen before:'
+                    email_text = '\n'.join([intro_text] + releases_text)
 
-              send_mail(
-                  'New music releases',
-                  email_text,
-                  'nik.kholin@gmail.com',
-                  [user.email],
-                  fail_silently=False,
-              )
+                    send_mail(
+                        'New music releases',
+                        email_text,
+                        'nik.kholin@gmail.com',
+                        [user.email],
+                        fail_silently=False,
+                    )
 
-              notification.last_sent_at = datetime.datetime.now()
-              notification.save()
-            else:
-              self.stdout.write(self.style.SUCCESS(f'no new releases {user.email}'))
+                    notification.last_sent_at = datetime.datetime.now()
+                    notification.save()
+                else:
+                    self.stdout.write(self.style.SUCCESS(f'no new releases {user.email}'))
 
 
             # send a telegram message
-            notification = user.notification_set.get(channel='telegram')
+            if user.notification_set.filter(channel='telegram').exists():
+                notification = user.notification_set.get(channel='telegram')
+                new_since = notification.last_sent_at
+                    # new_since = datetime.date.today() - datetime.timedelta(days=30)
+                new_releases = Release.objects.filter(
+                    artist__integration_id=integration.id,
+                    date__gte=new_since
+                ).order_by('-date')
 
-            if new_releases.exists():
-              self.stdout.write(self.style.SUCCESS(f'sending a telegram message for {notification.channel_id} '))
+                if new_releases.exists():
+                    self.stdout.write(self.style.SUCCESS(f'sending a telegram message for {notification.channel_id} '))
 
-              releases_text = [f'{release.date}: {release.artist.name} - {release.title}' for release in new_releases]
-              intro_text = 'Here are latest music releases that you have not seen before:'
-              text = '\n'.join([intro_text] + releases_text)
+                    releases_text = [f'{release.date}: {release.artist.name} - {release.title}' for release in new_releases]
+                    intro_text = 'Here are latest music releases that you have not seen before:'
+                    text = '\n'.join([intro_text] + releases_text)
 
-              bot_key = os.environ.get('TELEGRAM_API_KEY')
-              chat_id = notification.channel_id
-              send_message_url = f'https://api.telegram.org/bot{bot_key}/sendMessage?chat_id={chat_id}&text={text}'
-              requests.post(send_message_url)
+                    bot_key = os.environ.get('TELEGRAM_API_KEY')
+                    chat_id = notification.channel_id
+                    send_message_url = f'https://api.telegram.org/bot{bot_key}/sendMessage?chat_id={chat_id}&text={text}'
+                    requests.post(send_message_url)
 
-              # update last sent at
-              notification.last_sent_at = datetime.datetime.now()
-              notification.save()
-            else:
-              self.stdout.write(self.style.SUCCESS(f'no new releases {notification.channel_id}'))
+                    # update last sent at
+                    notification.last_sent_at = datetime.datetime.now()
+                    notification.save()
+                else:
+                    self.stdout.write(self.style.SUCCESS(f'no new releases {notification.channel_id}'))
